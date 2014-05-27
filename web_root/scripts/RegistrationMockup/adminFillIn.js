38f4d66e5e9237e3c55ab2804891c1081f60cf8f
Nathan Jones <nathan.jones@ironmail.org>
2014-05-27 10:20:32 -0600

    Renamed reg.html -> reg-parent.html and created reg-admin.html. Create adminFillIn.js file, which will handle populating the data from the staging table into the form.

---
+++ web_root/scripts/RegistrationMockup/adminFillIn.js
@@ -0,0 +1,19 @@
+/*global $, define*/
+
+define(function () {
+    'use strict';
+
+    /**
+     * @type {Object}
+     */
+    return {
+
+        fillInFields: function () {
+            
+        },
+
+        main: function () {
+            this.fillInFields;
+        }
+    };
+});
