e741a6b6dd3a9818823e3a0ffe18ad6ae3805858
Nathan Jones <nathan.jones@ironmail.org>
2014-07-14 16:44:03 -0600

    Fixed issue #58.

--- web_root/scripts/reg-parent/js/datepicker.js
+++ web_root/scripts/reg-parent/js/datepicker.js
@@ -10,7 +10,7 @@ define(function () {
         bindDatepickers: function () {
             var nowTemp = new Date();
             var twoYearsAgo = new Date(nowTemp.getFullYear() - 2, nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-            var twentyYearsAgo = new Date(nowTemp.getFullYear() - 20, nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+            var twentyYearsAgo = new Date(nowTemp.getFullYear() - 22, nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
 
             $('.dateinput').datepicker({
                 autoclose: true,
