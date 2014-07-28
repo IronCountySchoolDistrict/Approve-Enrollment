22bb7f852f8676e665de08b1dfb802821689e68d
Nathan Jones <nathan.jones@ironmail.org>
2014-07-28 17:55:33 -0600

    Made some progress on populating data from the server into the admin 'enroll student' page.

--- web_root/scripts/reg-admin/js/populate.js
+++ web_root/scripts/reg-admin/js/populate.js
@@ -1,12 +1,75 @@
 /*define, $j*/
 
-define(function() {
+define(function () {
     'use strict';
     return {
         main: function () {
 
         },
 
+        /**
+         * See enroll.js
+         */
+        convertBooleanToEnglish: function (value) {
+            return value ? "Yes" : "No";
+        },
 
+        /**
+         * Populate staging data into page (excluding emergency contacts and siblings data)
+         *
+         * @param stagingData {Object} - Data that came from the staging table
+         * that will be populated in this form (does not include emergency
+         * contacts or siblings data)
+         */
+        populateStaging: function (stagingData) {
+            $j('#first-name-field-value').html(stagingData.firstName);
+            $j('#middle-name-field-value').html(stagingData.middleName);
+            $j('#last-name-field-value').html(stagingData.lastName);
+
+            $j('#nickname-field-value').html(stagingData.nickname);
+            $j('#ssn-field-value').html(stagingData.ssn);
+            $j('#gender-field-value').html(stagingData.gender);
+            $j('#email-address-field-value').html(stagingData.emailAddress);
+            $j('#last-attended-grade-field-value').html(stagingData.lastAttendedGrade);
+            $j('#current-grade-field-value').html(stagingData.currentGrade);
+            $j('#birthdate-field-value').html(stagingData.birthdate);
+            $j('#home-phone-number-field-value').html(stagingData.homePhoneNumber);
+            $j('#student-cell-number-field-value').html(stagingData.studentCellNumber);
+
+            $j('#mailing-address-field-value').html(stagingData.mailingAddress + ' ' +
+                stagingData.mailingCity + ' ' +
+                stagingData.mailingState + ' ' +
+                stagingData.mailingZip);
+
+            $j('#residence-address-field-value').html(stagingData.residenceAddress + ' ' +
+                stagingData.residenceAddress + ' ' +
+                stagingData.residenceAddress + ' ' +
+                stagingData.residenceAddress);
+
+            $j('#school-city-field-value').html(stagingData.schoolCity);
+            $j('#school-state-field-value').html(stagingData.schoolState);
+            $j('#school-phone-field-value').html(stagingData.schoolPhone);
+            $j('#school-fax-field-value').html(stagingData.schoolFax);
+
+            $j('#prev-icsd-field-value').html(stagingData.prevIcsd ? "Yes" : "No");
+            $j('#prev-utah-field-value').html(stagingData.prevUtah ? "Yes" : "No");
+            $j('#count-terr-birth-field-value').html(stagingData.countTerrBirth);
+            $j('#date-first-enrolled-field-value').html(stagingData.dateFirstEnrolled);
+            $j('#three-ac-yrs-field-value').html(stagingData.threeAcYrs);
+            $j('#completed-grades-field-value').html(stagingData.completedGrades);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+            $j('#outside-us-field-value').html(stagingData.outsideUs);
+
+        }
     }
 });
