e92bcb00fa1f03e456b4460591e5d59512f61baf
Nathan Jones <purkinje90@gmail.com>
2015-03-04 14:48:56 -0700

    The rest of the removals of new student registration form

---
+++ web_root/scripts/registration/js/handlebars-helpers.js
@@ -0,0 +1,8 @@
+define(function() {
+    Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+        if (v1 === v2) {
+            return options.fn(this);
+        }
+        return options.inverse(this);
+    });
+});
